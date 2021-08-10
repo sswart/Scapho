7e7bd7f89dece50bd04907a48ff31c0425f310ec
Jesse Swart <jswart@xpirit.com>
2021-08-10 17:19:57 +0200

    feat(monthly-savings): basic monthly savings logic

---
+++ src/Scapho.Application/MonthlySavings.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Scapho.Application
+{
+    public class MonthlySavings
+    {
+        private void SaveAmount(int amount)
+        {
+            Amount = amount;
+        }
+
+        public Task SetAsync(int amount)
+        {
+            if (amount < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Should be bigger than 0");
+            }
+            SaveAmount(amount);
+            return Task.CompletedTask;
+        }
+
+        public Task<int> GetAsync() => Task.FromResult(Amount);
+
+        private int Amount { get; set; }
+    }
+}
