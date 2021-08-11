730b193726fb24233ea60f871a5c8a9f4951729b
Jesse Swart <jswart@xpirit.com>
2021-08-11 13:07:11 +0200

    feat(monthly-savings): monthly savings endpoint

---
+++ src/Scapho.Api/Controllers/SettingsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Scapho.Api.DTO;
+using Scapho.Application;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Scapho.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SettingsController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> IndexAsync([FromServices]MonthlySavings monthlySavings, [FromBody]SettingsDTO settings)
+        {
+            await monthlySavings.SetAsync(settings.MonthlySavingsAmount);
+            return Ok();
+        }
+    }
+}
